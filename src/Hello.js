0ca15a70d9a6dfac74875d3a744d1f5082ea8c6b
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2018-05-14 13:59:21 +0100

    style elements and add animation

--- src/Hello.js
+++ src/Hello.js
@@ -1,14 +1,66 @@
 import React from "react";
 import styled from "styled-components";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import "./App.css";
 
+const Div = styled.div`
+  position: absolute;
+  background: #ffffff;
+  box-shadow: 0px 0px 35px rgba(48, 95, 129, 0.1);
+  padding: 20px;
+  min-width: 140px;
+  right: 230px;
+  p {
+    margin: 0;
+  }
+`;
+const styles = {
+  buttonStyle: {
+    padding: "0 16px",
+    height: "45px",
+    borderRadius: "100px",
+    background: "#6067f1",
+    border: "1px solid #6067f1",
+    color: "#ffffff",
+    fontSize: "16px",
+    outline: "none",
+    cursor: "pointer"
+  },
+  contatiner: {
+    position: "relative"
+  }
+};
 class Button extends React.Component {
-  state = {
-    showDropdown: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDropdown: false
+    };
+  }
+
+  toggleDropdown = () => {
+    this.setState({
+      showDropdown: !this.state.showDropdown
+    });
   };
   render() {
+    const { showDropdown } = this.state;
     return (
-      <div>
-        <button>Click Me</button>
+      <div style={styles.container}>
+        <button style={styles.buttonStyle} onClick={this.toggleDropdown}>
+          Click Me
+        </button>
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout="300"
+          transitionLeaveTimeout="300"
+        >
+          {showDropdown ? (
+            <Div key="one">
+              <p>Hello world</p>
+            </Div>
+          ) : null}
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
+import React from "react";
+import { render } from "react-dom";
+import Button from "./Hello";
 
 const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
+  fontFamily: "sans-serif",
+  textAlign: "center",
+  position: "relative"
 };
 
 const App = () => (
   <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+    <Button transitionClass="example" />
   </div>
 );
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
