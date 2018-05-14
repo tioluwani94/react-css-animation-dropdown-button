6f739a23bf5b21c578cc97399b2c7d5f07cd22ac
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2018-05-14 15:45:19 +0100

    create accordion component with animation

---
+++ src/Accordion.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import styled from "styled-components";
+
+const Item = styled.a`
+  border-bottom: 1px solid #f8f8f8;
+  display: block;
+  cursor: pointer;
+
+  .description {
+    padding-bottom: 16px;
+  }
+`;
+class AccordionItem extends React.Component {
+  state = {
+    open: false
+  };
+
+  toggleOpen = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
+
+  render() {
+    const { data: { title, description }, count } = this.props;
+    return (
+      <Item onClick={this.toggleOpen}>
+        <p className="title">{title}</p>
+        <ReactCSSTransitionGroup
+          transitionName="show-accordion"
+          transitionEnterTimeout="250"
+          transitionLeaveTimeout="250"
+        >
+          {this.state.open ? (
+            <div className="description" key={count}>
+              <p>{description}</p>
+            </div>
+          ) : null}
+        </ReactCSSTransitionGroup>
+      </Item>
+    );
+  }
+}
+class Accordion extends React.Component {
+  render() {
+    const { items } = this.props;
+    return (
+      <div>
+        {items.map((item, i) => (
+          <AccordionItem key={i.toString()} data={item} count={i.toString()} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Accordion;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import Button from "./Hello";
+import Accordion from "./Accordion";
 
 const styles = {
   fontFamily: "sans-serif",
@@ -8,9 +9,23 @@ const styles = {
   position: "relative"
 };
 
+const items = [
+  {
+    title: "Section One",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    title: "Section Two",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  }
+];
+
 const App = () => (
   <div style={styles}>
     <Button transitionClass="example" />
+    <Accordion items={items} />
   </div>
 );
 
