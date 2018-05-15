3f4dad16b193f23c2cb7b3a72810c8dc0d04d6dd
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2018-05-15 15:35:17 +0100

    add collapse icon for accordion item

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import ReactTransitionGroup from "react-addons-transition-group";
 import styled from "styled-components";
 
 const Item = styled.a`
@@ -9,24 +10,40 @@ const Item = styled.a`
   text-align: left;
 
   .title {
+    position: relative;
     display: flex;
     align-items: center;
     justify-content: space-between;
+    padding: 8px 0;
 
     span.icon {
       background: #000000;
-      width: 15px;
+      width: 12px;
       height: 2px;
+      position: absolute;
+      display: block;
+      right: 0;
+    }
+    span.icon2 {
+      background: #000000;
+      width: 2px;
+      height: 12px;
+      position: absolute;
+      display: block;
+      right: 5px;
+      top: 11px;
     }
   }
 
   .description {
-    padding-bottom: 16px;
+    padding-bottom: 1rem;
   }
 `;
 class AccordionItem extends React.Component {
   state = {
-    open: false
+    open: false,
+    showIcon: true,
+    showIcon2: true
   };
 
   toggleOpen = () => {
@@ -37,20 +54,29 @@ class AccordionItem extends React.Component {
 
   render() {
     const { data: { title, description }, count } = this.props;
+    const { showIcon, showIcon2 } = this.state;
     return (
       <Item onClick={this.toggleOpen}>
-        <p className="title">
-          <span>{title}</span>
-          <span class="icon" />
-        </p>
         <ReactCSSTransitionGroup
           transitionName="show-accordion"
-          transitionEnterTimeout="250"
-          transitionLeaveTimeout="250"
+          transitionEnterTimeout="300"
+          transitionLeaveTimeout="150"
+          component="div"
         >
+          <p className="title">
+            <span>{title}</span>
+            <span class="icon" />
+            <ReactCSSTransitionGroup
+              transitionName="icon"
+              transitionEnterTimeout="50"
+              transitionLeaveTimeout="50"
+            >
+              {!this.state.open ? <span class="icon2" /> : null}
+            </ReactCSSTransitionGroup>
+          </p>
           {this.state.open ? (
             <div className="description" key={count}>
-              <p>{description}</p>
+              {description}
             </div>
           ) : null}
         </ReactCSSTransitionGroup>
